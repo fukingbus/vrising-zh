7f540ef6c89957c1cc17b6a5708df2b89191c8c3
Kenneth <4240628+fukingbus@users.noreply.github.com>
2022-05-24 07:12:29 +0800

    added text replacement

--- index.js
+++ index.js
@@ -2,46 +2,80 @@
 const projectId = "translate-cf9a4"
 const RAW_FILE = 'SChinese.json'
 const TRANSLATED_FILE = 'SChinese.json'
+const REPLACE_DICT = 'dict.json'
 
 //
-const keys = require('./keys.json');
 const fs = require('fs')
+const keys = require('./keys.json');
 const cliProgress = require('cli-progress');
+//
+const replacementDict = JSON.parse(fs.readFileSync(`./${REPLACE_DICT}`))
 //
 const {Translate} = require('@google-cloud/translate').v2
 const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
 const translate = new Translate({projectId,credentials: keys})
+//
 
-async function quickStart() {
+async function entry() {
     if(!fs.existsSync('./input'))
         fs.mkdirSync('./input')
         
     if(!fs.existsSync('./output'))
         fs.mkdirSync('./output')
 
-    let raw = JSON.parse(fs.readFileSync(`./input/${RAW_FILE}`))
-    let translated = JSON.parse(fs.readFileSync(`./input/${TRANSLATED_FILE}`))
-    if(raw.Nodes.length != translated.Nodes.length){
-        let tmpTranslated = raw
+    const raw = JSON.parse(fs.readFileSync(`./input/${RAW_FILE}`))
+    const translated = fs.existsSync(`./output/${TRANSLATED_FILE}`) ? JSON.parse(fs.readFileSync(`./output/${TRANSLATED_FILE}`)) : null
 
-        progressBar.start(raw.Nodes.length,0)
+    if(translated == null && raw.Nodes.length != translated.Nodes.length){
+        console.log('[SYS] Begin Translation ')
+        await doTranslate(raw)
+    }
+    console.log('[SYS] Begin Replacement')
+    if(translated != null)
+        await doReplace()
+    else
+        console.log('[SYS] Translated file missing')
+  }
 
-        for (let index = 0; index < raw.Nodes.length; index++) {
-            const rawText = raw.Nodes[index].Text;
-            const translatedText = (await translate.translate(rawText, {
-                from: 'zh-CN',
-                to: 'zh-TW'
-            }))[0];
-            tmpTranslated.Nodes[index].Text = translatedText
-            progressBar.increment()
+  async function doTranslate(raw){
+    let tmpTranslated = raw
+
+    progressBar.start(raw.Nodes.length,0)
+
+    for (let index = 0; index < raw.Nodes.length; index++) {
+        const rawText = raw.Nodes[index].Text;
+        const translatedText = (await translate.translate(rawText, {
+            from: 'zh-CN',
+            to: 'zh-TW'
+        }))[0];
+        tmpTranslated.Nodes[index].Text = translatedText
+        progressBar.increment()
+    }
+    progressBar.stop()
+    await output(tmpTranslated,`./output/${TRANSLATED_FILE}`)
+    return tmpTranslated
+  }
+
+  async function doReplace(){
+    const translated = JSON.parse(fs.readFileSync(`./output/${TRANSLATED_FILE}`))
+    let tmpTranslated = translated
+
+    progressBar.start(tmpTranslated.Nodes.length,0)
+
+    for (let index = 0; index < tmpTranslated.Nodes.length; index++) {
+        for (let searchIndex = 0; searchIndex < replacementDict.entries.length; searchIndex++) {
+            const searchEntry = replacementDict.entries[searchIndex];
+            tmpTranslated.Nodes[index].Text = tmpTranslated.Nodes[index].Text.replace(new RegExp(searchEntry,'g'), replacementDict.replacement[searchIndex])
         }
-        fs.writeFileSync(`./output/${TRANSLATED_FILE}`,JSON.stringify(tmpTranslated,null,4))
-        progressBar.stop()
+
+        progressBar.increment()
     }
-    else{
-        console.log('[SYS] Terminated (same length)')
-    }
+    progressBar.stop()
+    output(tmpTranslated,`./output/${TRANSLATED_FILE}`)
+  }
+
+  async function output(payload,path){
+    await fs.writeFileSync(path,JSON.stringify(payload,null,4))
   }
   
-  quickStart();
+  entry()
